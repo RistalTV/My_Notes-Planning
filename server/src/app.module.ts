236819411ae014cb4816fcdc6245c27c9606ba24
Leonid <ristalvtgames@yandex.com>
2022-11-11 02:14:59 +0300

    init

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: config.get<'aurora-data-api'>('TYPEORM_CONNECTION') as 'mysql',
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        database: config.get<string>('TYPEORM_DB'),
+        host: config.get<string>('TYPEORM_HOST'),
+        port: config.get<number>('TYPEORM_PORT'),
+        entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
+        //synchronize: true,
+        autoLoadEntities: true,
+        logging: 'all',
+        logger: 'file',
+      }),
+    }),
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+      playground: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,15 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function start() {
-    const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
+  const config = await app.get(ConfigService);
+  const PORT = config.get<number>('API_PORT');
 
-    await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
+  await app.listen(PORT || 5000, () =>
+    console.log(`Server started on port = ${PORT}`),
+  );
 }
 
 start();
